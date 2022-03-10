a1575971ea7d4c4b14695b8b9c86db99c4939fc9
Haoyuan Kevin Xia <41046146+kevin062397@users.noreply.github.com>
2022-03-10 00:10:27 -0500

    Encapsulate points and corners into classes

--- image_analyzer.py
+++ image_analyzer.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import os
 from typing import Tuple, Union
+from utilities import Point, DiagonalCorners, QuadrilateralCorners
 
 
 def get_normalized(array: np.ndarray, output_min=0.0, output_max=1.0) -> np.ndarray:
@@ -14,112 +15,115 @@ def get_normalized(array: np.ndarray, output_min=0.0, output_max=1.0) -> np.ndar
         return np.array(array)
 
 
-def get_corners(binary_2d_array: np.ndarray, target_value=255) -> Tuple[
-        Tuple[int, int], Tuple[int, int], Tuple[int, int], Tuple[int, int]]:
+def get_corners(binary_2d_array: np.ndarray, target_value=255) -> QuadrilateralCorners:
     if len(binary_2d_array.shape) != 2:
         raise ValueError("Input array must by 2D")
 
     rows = binary_2d_array.shape[0]
     cols = binary_2d_array.shape[1]
 
-    top_corner = None
-    bottom_corner = None
-    left_corner = None
-    right_corner = None
+    top_left_corner = None
+    top_right_corner = None
+    bottom_left_corner = None
+    bottom_right_corner = None
 
-    # Get top corner
+    # Get top-right corner
     for row in range(0, rows, 1):
         for col in range(0, cols, 1):
             if binary_2d_array[row][col] == target_value:
-                top_corner = (col, row)  # (x, y) = (col, row)
+                top_right_corner = Point(col, row)  # (x, y) = (col, row)
                 # Break from inner loop
                 break
-        if top_corner is not None:
+        if top_right_corner is not None:
             # Break from outer loop
             break
 
-    # Get bottom corner
+    # Get bottom-left corner
     for row in range(rows - 1, -1, -1):
         for col in range(cols - 1, -1, -1):
             if binary_2d_array[row][col] == target_value:
-                bottom_corner = (col, row)  # (x, y) = (col, row)
+                bottom_left_corner = Point(col, row)  # (x, y) = (col, row)
                 # Break from inner loop
                 break
-        if bottom_corner is not None:
+        if bottom_left_corner is not None:
             # Break from outer loop
             break
 
-    # Get left corner
+    # Get top-left corner
     for col in range(0, cols, 1):
         for row in range(0, rows, 1):
             if binary_2d_array[row][col] == target_value:
-                left_corner = (col, row)  # (x, y) = (col, row)
+                top_left_corner = Point(col, row)  # (x, y) = (col, row)
                 # Break from inner loop
                 break
-        if left_corner is not None:
+        if top_left_corner is not None:
             # Break from outer loop
             break
 
-    # Get right corner
+    # Get bottom-right corner
     for col in range(cols - 1, -1, -1):
         for row in range(rows - 1, -1, -1):
             if binary_2d_array[row][col] == target_value:
-                right_corner = (col, row)  # (x, y) = (col, row)
+                bottom_right_corner = Point(col, row)  # (x, y) = (col, row)
                 # Break from inner loop
                 break
-        if right_corner is not None:
+        if bottom_right_corner is not None:
             # Break from outer loop
             break
 
-    if top_corner is None or bottom_corner is None or left_corner is None or right_corner is None:
+    if top_left_corner is None or top_right_corner is None or bottom_left_corner is None or bottom_right_corner is None:
         raise ValueError("The target value is not found in the input")
 
-    return top_corner, bottom_corner, left_corner, right_corner
+    return QuadrilateralCorners(top_left_corner, top_right_corner, bottom_left_corner, bottom_right_corner)
 
 
-def get_crop_points_with_corners(corners: Tuple[Tuple[int, int], Tuple[int, int], Tuple[int, int], Tuple[int, int]],
-                                 mode: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+def get_crop_points_with_corners(corners: QuadrilateralCorners, mode: int) -> DiagonalCorners:
     # Mode 0: cropped with circumscribed rectangle
     # Mode 1: cropped with inscribed rectangle
 
-    corner_list = [corner for corner in corners]
+    corner_list = [
+        corners.top_left,
+        corners.top_right,
+        corners.bottom_left,
+        corners.bottom_right
+    ]
+
+    from_point = Point()
+    to_point = Point()
 
     # Sort corners by x
-    corner_list.sort(key=lambda x: x[0])
+    corner_list.sort(key=lambda point: point.x)
     if mode == 0:
-        from_x = corner_list[0][0]
-        to_x = corner_list[-1][0]
+        from_point.x = corner_list[0].x
+        to_point.x = corner_list[-1].x
     else:
-        from_x = corner_list[1][0]
-        to_x = corner_list[-2][0]
+        from_point.x = corner_list[1].x
+        to_point.x = corner_list[-2].x
 
     # Sort corners by y
-    corner_list.sort(key=lambda x: x[1])
+    corner_list.sort(key=lambda point: point.y)
     if mode == 0:
-        from_y = corner_list[0][1]
-        to_y = corner_list[-1][1]
+        from_point.y = corner_list[0].y
+        to_point.y = corner_list[-1].y
     else:
-        from_y = corner_list[1][1]
-        to_y = corner_list[-2][1]
+        from_point.y = corner_list[1].y
+        to_point.y = corner_list[-2].y
 
-    return (from_x, from_y), (to_x, to_y)
+    return DiagonalCorners(from_point, to_point)
 
 
-def get_crop_preview(image: np.ndarray, crop_points: Tuple[Tuple[int, int], Tuple[int, int]],
-                     color: Union[int, Tuple[int, int, int]], thickness: int) -> np.ndarray:
+def get_crop_preview(image: np.ndarray, crop_points: DiagonalCorners, color: Union[int, Tuple[int, int, int]], thickness: int) -> np.ndarray:
     result_image = np.array(image)
-    cv.rectangle(result_image, (crop_points[0][0], crop_points[0][1]), (crop_points[1][0], crop_points[1][1]), color,
-                 thickness=thickness)
+    cv.rectangle(result_image, crop_points.point1.get_tuple(), crop_points.point2.get_tuple(), color, thickness=thickness)
     return result_image
 
 
-def get_cropped(image: np.ndarray, crop_points: Tuple[Tuple[int, int], Tuple[int, int]]) -> np.ndarray:
+def get_cropped(image: np.ndarray, crop_points: DiagonalCorners) -> np.ndarray:
     result_image = np.array(image)
-    return result_image[crop_points[0][1]:crop_points[1][1], crop_points[0][0]:crop_points[1][0]]
+    return result_image[crop_points.point1.y:crop_points.point2.y, crop_points.point1.x:crop_points.point2.x]
 
 
-def single_frame_processing(input_directory_path, input_file_name):
-
+def process_single_frame(input_directory_path, input_file_name):
     input_file_path = os.path.join(input_directory_path, input_file_name)
 
     # Read image
@@ -137,36 +141,31 @@ def single_frame_processing(input_directory_path, input_file_name):
     # Reduce Image Noise
     # # Gaussian Blur
     # img_8_bit_avg = cv.GaussianBlur(img_8_bit_int, (5,5), 0)
-    # cv.imshow('Gaussian Blur', img_8_bit_avg)
+    # cv.imshow("Gaussian Blur", img_8_bit_avg)
 
     # Bilateral
-    img_8_bit_avg = cv.bilateralFilter(img_8_bit_int, 5, 10, 10)
-    cv.imshow('Bilateral', img_8_bit_avg)
+    img_8_bit_bilat = cv.bilateralFilter(img_8_bit_int, 5, 10, 10)
+    cv.imshow("Bilateral", img_8_bit_bilat)
 
     cv.waitKey(0)
 
     # Edge detection
-    canny = cv.Canny(img_8_bit_avg, 100, 200)
+    canny = cv.Canny(img_8_bit_bilat, 100, 200)
     cv.imshow("Canny Edge Detection", canny)
 
     # Find the four corners
     corners = get_corners(canny)
-    top_corner, bottom_corner, left_corner, right_corner = corners
-    print("Top: {}".format(top_corner))
-    print("Botton: {}".format(bottom_corner))
-    print("Left: {}".format(left_corner))
-    print("Right: {}".format(right_corner))
+    print(corners)
 
     # Crop
     crop_points = get_crop_points_with_corners(corners, mode=0)
     crop_preview = get_crop_preview(
-        img_8_bit_avg, crop_points, color=0, thickness=1)
+        img_8_bit_bilat, crop_points, color=0, thickness=1)
     cv.imshow("Crop Preview", crop_preview)
-    img_cropped = get_cropped(img_8_bit_avg, crop_points)
+    img_cropped = get_cropped(img_8_bit_bilat, crop_points)
     cv.imshow("Cropped", img_cropped)
 
     # Find Pores
-
 
     # References
     # https://docs.opencv.org/4.x/d1/d5c/tutorial_py_kmeans_opencv.html
@@ -179,27 +178,25 @@ def single_frame_processing(input_directory_path, input_file_name):
     center = np.uint8(center)
     res = center[label.flatten()]
     res2 = res.reshape(img_cropped.shape)
-    cv.imshow('res2', res2)
+    cv.imshow("Result", res2)
 
     # Find Matrix
 
     # Find Reinforcement
 
-    
-
     cv.waitKey(0)
 
 
 def main():
-
-    input_directory_path = "Y://APS//2020-3_1IDC//tomo//32bit//sample_1//hassani_sam1_load0_tomo//"
+    input_directory_path = r"Y:\APS\2020-3_1IDC\tomo\32bit\sample_1\hassani_sam1_load0_tomo"
+    # input_directory_path = r"/Users/haoyuanxia/Desktop"
     output_directory_path = r"Y:\APS\2021-3_1IDC\WAXS_fitting\mapping\He_1-1_m5\90deg"
 
     input_file_names = [f for f in os.listdir(input_directory_path) if (
         os.path.isfile(os.path.join(input_directory_path, f)) and f.endswith(".tiff"))]
 
     for input_file_name in input_file_names:
-        single_frame_processing(input_directory_path, input_file_name)
+        process_single_frame(input_directory_path, input_file_name)
 
 
 if __name__ == "__main__":
---
+++ utilities.py
@@ -0,0 +1,34 @@
+from typing import Tuple, Union
+
+
+class Point:
+    def __init__(self, x: Union[int, float] = 0, y: Union[int, float] = 0) -> None:
+        self.x = x
+        self.y = y
+
+    def __str__(self) -> str:
+        return "({}, {})".format(self.x, self.y)
+
+    def get_tuple(self) -> Tuple[Union[int, float], Union[int, float]]:
+        return self.x, self.y
+
+
+class DiagonalCorners:
+    def __init__(self, point1: Point = Point(), point2: Point = Point()) -> None:
+        self.point1 = point1
+        self.point2 = point2
+    
+    def __str__(self) -> str:
+        return "Point 1: {}\nPoint 2: {}".format(self.point1, self.point2)
+
+
+
+class QuadrilateralCorners:
+    def __init__(self, top_left: Point = Point(), top_right: Point = Point(), bottom_left: Point = Point(), bottom_right: Point = Point()) -> None:
+        self.top_left = top_left
+        self.top_right = top_right
+        self.bottom_left = bottom_left
+        self.bottom_right = bottom_right
+
+    def __str__(self) -> str:
+        return "Top-left: {}\nTop-right: {}\nBottom-left: {}\nBottom-right: {}".format(self.top_left, self.top_right, self.bottom_left, self.bottom_right)
