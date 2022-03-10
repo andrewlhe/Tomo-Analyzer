3b39d8a6ea4caba86b1023c01c3c4db9c06ba4ae
Haoyuan Kevin Xia <41046146+kevin062397@users.noreply.github.com>
2022-03-10 00:44:05 -0500

    Reorganize code

--- image_analyzer.py
+++ image_analyzer.py
@@ -2,112 +2,11 @@ import cv2 as cv
 import numpy as np
 import os
 from typing import Union
-from utilities import Point, DiagonalCorners, QuadrilateralCorners
+import utilities
+from utilities import DiagonalCorners, QuadrilateralCorners
 
 
 DEBUG = True
-
-
-def get_normalized(array: np.ndarray, output_min=0.0, output_max=1.0) -> np.ndarray:
-    input_min = np.min(array)
-    input_max = np.max(array)
-    if input_min != output_max:
-        scale_factor = (output_max - output_min) / (input_max - input_min)
-        return (array - input_min) * scale_factor + output_min
-    else:
-        return np.array(array)
-
-
-def get_corners(binary_2d_array: np.ndarray, target_value=255) -> QuadrilateralCorners:
-    if len(binary_2d_array.shape) != 2:
-        raise ValueError("Input array must by 2D")
-
-    rows = binary_2d_array.shape[0]
-    cols = binary_2d_array.shape[1]
-
-    top_left_corner = None
-    top_right_corner = None
-    bottom_left_corner = None
-    bottom_right_corner = None
-
-    # Get top-right corner
-    for row in range(0, rows, 1):
-        for col in range(0, cols, 1):
-            if binary_2d_array[row][col] == target_value:
-                top_right_corner = Point(col, row)  # (x, y) = (col, row)
-                # Break from inner loop
-                break
-        if top_right_corner is not None:
-            # Break from outer loop
-            break
-
-    # Get bottom-left corner
-    for row in range(rows - 1, -1, -1):
-        for col in range(cols - 1, -1, -1):
-            if binary_2d_array[row][col] == target_value:
-                bottom_left_corner = Point(col, row)  # (x, y) = (col, row)
-                # Break from inner loop
-                break
-        if bottom_left_corner is not None:
-            # Break from outer loop
-            break
-
-    # Get top-left corner
-    for col in range(0, cols, 1):
-        for row in range(0, rows, 1):
-            if binary_2d_array[row][col] == target_value:
-                top_left_corner = Point(col, row)  # (x, y) = (col, row)
-                # Break from inner loop
-                break
-        if top_left_corner is not None:
-            # Break from outer loop
-            break
-
-    # Get bottom-right corner
-    for col in range(cols - 1, -1, -1):
-        for row in range(rows - 1, -1, -1):
-            if binary_2d_array[row][col] == target_value:
-                bottom_right_corner = Point(col, row)  # (x, y) = (col, row)
-                # Break from inner loop
-                break
-        if bottom_right_corner is not None:
-            # Break from outer loop
-            break
-
-    if top_left_corner is None or top_right_corner is None or bottom_left_corner is None or bottom_right_corner is None:
-        raise ValueError("The target value is not found in the input")
-
-    return QuadrilateralCorners(top_left_corner, top_right_corner, bottom_left_corner, bottom_right_corner)
-
-
-def get_crop_points_with_corners(corners: QuadrilateralCorners, mode: int) -> DiagonalCorners:
-    # Mode 0: cropped with circumscribed rectangle
-    # Mode 1: cropped with inscribed rectangle
-
-    corner_list = corners.get_list()
-
-    from_point = Point()
-    to_point = Point()
-
-    # Sort corners by x
-    corner_list.sort(key=lambda point: point.x)
-    if mode == 0:
-        from_point.x = corner_list[0].x
-        to_point.x = corner_list[-1].x
-    else:
-        from_point.x = corner_list[1].x
-        to_point.x = corner_list[-2].x
-
-    # Sort corners by y
-    corner_list.sort(key=lambda point: point.y)
-    if mode == 0:
-        from_point.y = corner_list[0].y
-        to_point.y = corner_list[-1].y
-    else:
-        from_point.y = corner_list[1].y
-        to_point.y = corner_list[-2].y
-
-    return DiagonalCorners(from_point, to_point)
 
 
 def get_corner_preview(image: np.ndarray, corners: QuadrilateralCorners, color: Union[int, tuple[int, int, int]], radius: int, thickness: int) -> np.ndarray:
@@ -138,7 +37,7 @@ def process_single_frame(input_directory_path, input_file_name):
     # cv.imshow("Original Image", img)
 
     # Normalization
-    img_normalized = get_normalized(img)
+    img_normalized = utilities.get_normalized(img)
     # cv.imshow("Normalized Image", img_normalized)
 
     # Convert from decimal to integer
@@ -162,14 +61,14 @@ def process_single_frame(input_directory_path, input_file_name):
         cv.imshow("Canny Edge Detection", canny)
 
     # Find the four corners
-    corners = get_corners(canny)
+    corners = utilities.get_corners(canny)
     if DEBUG:
         corner_preview = get_corner_preview(
             img_8_bit_bilat, corners, color=0, radius=4, thickness=2)
         cv.imshow("Corner Preview", corner_preview)
 
     # Crop
-    crop_points = get_crop_points_with_corners(corners, mode=0)
+    crop_points = utilities.get_crop_points_with_corners(corners, mode=0)
     if DEBUG:
         crop_preview = get_crop_preview(
             corner_preview, crop_points, color=0, thickness=2)
--- utilities.py
+++ utilities.py
@@ -1,3 +1,4 @@
+import numpy as np
 from typing import Union
 
 
@@ -40,3 +41,106 @@ class QuadrilateralCorners:
 
     def get_list(self) -> list[Point]:
         return [self.top_left, self.top_right, self.bottom_left, self.bottom_right]
+
+
+def get_normalized(array: np.ndarray, output_min=0.0, output_max=1.0) -> np.ndarray:
+    input_min = np.min(array)
+    input_max = np.max(array)
+    if input_min != output_max:
+        scale_factor = (output_max - output_min) / (input_max - input_min)
+        return (array - input_min) * scale_factor + output_min
+    else:
+        return np.array(array)
+
+
+def get_corners(binary_2d_array: np.ndarray, target_value=255) -> QuadrilateralCorners:
+    if len(binary_2d_array.shape) != 2:
+        raise ValueError("Input array must by 2D")
+
+    rows = binary_2d_array.shape[0]
+    cols = binary_2d_array.shape[1]
+
+    top_left_corner = None
+    top_right_corner = None
+    bottom_left_corner = None
+    bottom_right_corner = None
+
+    # Get top-right corner
+    for row in range(0, rows, 1):
+        for col in range(0, cols, 1):
+            if binary_2d_array[row][col] == target_value:
+                top_right_corner = Point(col, row)  # (x, y) = (col, row)
+                # Break from inner loop
+                break
+        if top_right_corner is not None:
+            # Break from outer loop
+            break
+
+    # Get bottom-left corner
+    for row in range(rows - 1, -1, -1):
+        for col in range(cols - 1, -1, -1):
+            if binary_2d_array[row][col] == target_value:
+                bottom_left_corner = Point(col, row)  # (x, y) = (col, row)
+                # Break from inner loop
+                break
+        if bottom_left_corner is not None:
+            # Break from outer loop
+            break
+
+    # Get top-left corner
+    for col in range(0, cols, 1):
+        for row in range(0, rows, 1):
+            if binary_2d_array[row][col] == target_value:
+                top_left_corner = Point(col, row)  # (x, y) = (col, row)
+                # Break from inner loop
+                break
+        if top_left_corner is not None:
+            # Break from outer loop
+            break
+
+    # Get bottom-right corner
+    for col in range(cols - 1, -1, -1):
+        for row in range(rows - 1, -1, -1):
+            if binary_2d_array[row][col] == target_value:
+                bottom_right_corner = Point(col, row)  # (x, y) = (col, row)
+                # Break from inner loop
+                break
+        if bottom_right_corner is not None:
+            # Break from outer loop
+            break
+
+    if top_left_corner is None or top_right_corner is None or bottom_left_corner is None or bottom_right_corner is None:
+        raise ValueError("The target value is not found in the input")
+
+    return QuadrilateralCorners(top_left_corner, top_right_corner, bottom_left_corner, bottom_right_corner)
+
+
+def get_crop_points_with_corners(corners: QuadrilateralCorners, mode: int) -> DiagonalCorners:
+    # Mode 0: cropped with circumscribed rectangle
+    # Mode 1: cropped with inscribed rectangle
+
+    corner_list = corners.get_list()
+
+    from_point = Point()
+    to_point = Point()
+
+    # Sort corners by x
+    corner_list.sort(key=lambda point: point.x)
+    if mode == 0:
+        from_point.x = corner_list[0].x
+        to_point.x = corner_list[-1].x
+    else:
+        from_point.x = corner_list[1].x
+        to_point.x = corner_list[-2].x
+
+    # Sort corners by y
+    corner_list.sort(key=lambda point: point.y)
+    if mode == 0:
+        from_point.y = corner_list[0].y
+        to_point.y = corner_list[-1].y
+    else:
+        from_point.y = corner_list[1].y
+        to_point.y = corner_list[-2].y
+
+    return DiagonalCorners(from_point, to_point)
+    
